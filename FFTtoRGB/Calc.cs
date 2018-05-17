7a5859d42751551fe7a9b93abb7a1dca328a3a0c
danielcunhaa <danielcunha54@gmail.com>
2018-05-17 15:06:37 -0300

    Map method's summary

--- FFTtoRGB/Calc.cs
+++ FFTtoRGB/Calc.cs
@@ -40,6 +40,15 @@ namespace FFTtoRGB
             return result;
         }
 
+        /// <summary>
+        /// Map method. Calculate the value in the range [from1, to1] in the new range [from2, to2]
+        /// </summary>
+        /// <param name="value">Value to be calculated</param>
+        /// <param name="from1">Initial floor</param>
+        /// <param name="to1">Initial ceiling</param>
+        /// <param name="from2">Final floor</param>
+        /// <param name="to2">Final ceiling</param>
+        /// <returns>Mapped value</returns>
         public static double Map(this double value, double from1, double to1, double from2, double to2)
         {
             return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
