7427438db013262ee9511c01690eb46ae9ea5569
danielcunhaa <danielcunha54@gmail.com>
2018-05-17 15:04:33 -0300

    Implemented extension method Map (like the one in Arduino)

--- FFTtoRGB/Calc.cs
+++ FFTtoRGB/Calc.cs
@@ -39,5 +39,10 @@ namespace FFTtoRGB
 
             return result;
         }
+
+        public static double Map(this double value, double from1, double to1, double from2, double to2)
+        {
+            return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+        }
     }
 }
