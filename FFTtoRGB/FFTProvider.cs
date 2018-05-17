f06d18a57b99dd129e2fd6b2e88c52e3ca85c71e
Daniel Cunha <32555455+danielcunhaa@users.noreply.github.com>
2018-05-17 09:27:19 -0300

    Update FFTProvider.cs

--- FFTtoRGB/FFTProvider.cs
+++ FFTtoRGB/FFTProvider.cs
@@ -6,7 +6,7 @@ using NAudio.Wave;
 namespace FFTtoRGB
 {
     /// <summary>
-    /// Provides de FFT array and its frequencies. For now, it's working with the WaveInEvent using
+    /// Provides the FFT array and its frequencies. For now, it's working with the WaveInEvent using
     /// the device number 0 (usually the microphone). Later, the idea is to get the audio direct of
     /// the Window's digital output.
     /// </summary>
