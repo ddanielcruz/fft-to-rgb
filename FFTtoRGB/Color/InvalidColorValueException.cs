82f50d5e1f6ab6712b99ce2244291e30cde8a9f5
danielcunhaa <danielcunha54@gmail.com>
2018-05-17 13:07:39 -0300

    Created RGB class and InvalidColorValueException

---
+++ FFTtoRGB/Color/InvalidColorValueException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace FFTtoRGB.Color
+{
+    /// <summary>
+    /// Invalid value for color exception
+    /// </summary>
+    public class InvalidColorValueException : Exception
+    {
+        public override string Message => "Invalid value. RGB colors must be between zero and 255.";
+
+        public InvalidColorValueException()
+        {
+        }
+        public InvalidColorValueException(string message) : base(message)
+        {
+        }
+        public InvalidColorValueException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+        protected InvalidColorValueException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ FFTtoRGB/Color/RGB.cs
@@ -0,0 +1,57 @@
+﻿namespace FFTtoRGB.Color
+{
+    /// <summary>
+    /// RGB color to pass to the Arduino UNO
+    /// </summary>
+    public class RGB
+    {
+        private uint _red;
+        public uint Red
+        {
+            get => _red;
+            set
+            {
+                if (value <= 255)
+                    _red = value;
+                else
+                    throw new InvalidColorValueException();
+            }
+        }
+
+        private uint _green;
+        public uint Green
+        {
+            get => _green;
+            set
+            {
+                if (value <= 255)
+                    _green = value;
+                else
+                    throw new InvalidColorValueException();
+            }
+        }
+
+        private uint _blue;
+        public uint Blue
+        {
+            get => _blue;
+            set
+            {
+                if (value <= 255)
+                    _blue = value;
+                else
+                    throw new InvalidColorValueException();
+            }
+        }
+
+        public RGB()
+        {
+        }
+        public RGB(uint red, uint green, uint blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+    }
+}
