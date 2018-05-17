33fcbe11849fc46e8ce30a2325310a0ac679f7b6
danielcunhaa <danielcunha54@gmail.com>
2018-05-16 23:46:11 -0300

    Created ConsoleSample to test the library

---
+++ ConsoleSample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using FFTtoRGB;
+
+namespace ConsoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prov = new FFTProvider();
+            prov.StartRecording();
+            
+            //var fft = prov.Read();
+            //prov.StopRecording();
+
+            var freq = prov.GetFreqArray();
+
+            //Console.WriteLine(fft.Length);
+            //Console.WriteLine(freq.Length);
+
+            //for (int i = 0; i < freq.Length; i += 8)
+            //{
+            //    Console.WriteLine($"[{fft[i]}, {freq[i]}]");
+            //}
+
+            for (int i = 0; i < 50; i++)
+            {
+                var FFT = prov.Read();
+                Console.WriteLine(FFT[0]);
+
+                Thread.Sleep(500);
+            }
+
+            prov.Dispose();
+            Console.ReadKey(true);
+        }
+    }
+}
