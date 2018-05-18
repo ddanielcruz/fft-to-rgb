94a6f298225285ec807e85273bc4eff08cd2214d
danielcunhaa <danielcunha54@gmail.com>
2018-05-18 10:52:22 -0300

    Enum moved

--- FFTtoRGB/Color/ColorSettings.cs
+++ FFTtoRGB/Color/ColorSettings.cs
@@ -56,17 +56,4 @@
             Y = y;
         }
     }
-
-    /// <summary>
-    /// Possible orders to distribute colors
-    /// </summary>
-    public enum Order
-    {
-        RGB = 0,
-        RBG = 1,
-        GRB = 2,
-        GBR = 3,
-        BRG = 4,
-        BGR = 5
-    }
 }
---
+++ FFTtoRGB/Color/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace FFTtoRGB.Color
+{
+    /// <summary>
+    /// Possible orders to distribute colors
+    /// </summary>
+    public enum Order
+    {
+        RGB = 0,
+        RBG = 1,
+        GRB = 2,
+        GBR = 3,
+        BRG = 4,
+        BGR = 5
+    }
+}
