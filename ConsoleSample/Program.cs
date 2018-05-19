8add935b343e58506b62d7b64eedc409dd8eda92
danielcunhaa <danielcunha54@gmail.com>
2018-05-19 10:40:36 -0300

    Created ConsoleSample to debug features

---
+++ ConsoleSample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Transmitter;
+
+namespace ConsoleSample
+{
+    class Program
+    {
+        static SerialTransmitter Transmitter;
+
+        static void Main(string[] args)
+        {
+            Transmitter = new SerialTransmitter("COM8");
+
+            try
+            {
+                Transmitter.Open();
+                Console.WriteLine("Connection is open.");
+
+                while (true)
+                {
+                    try
+                    {
+                        byte input = Convert.ToByte(Console.ReadLine());
+                        if (input == 0)
+                            break;
+
+                        Transmitter.Write(input);
+                    }
+                    catch (OverflowException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+
+                Console.WriteLine("Saindo...");
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine(e.Message);                
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
