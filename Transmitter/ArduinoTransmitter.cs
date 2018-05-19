c262e9cff2f7a4f04013a5b8bf8a496025a5c533
andregasoli <andresichelero@outlook.com>
2018-05-18 21:09:43 -0300

    Initialized Transmitter library project

---
+++ Transmitter/ArduinoTransmitter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace Transmitter
+{
+    /// <summary>
+    /// Class to send data to the arduino
+    /// </summary>
+    public class ArduinoTransmitter
+    {
+        /// <summary>
+        /// Serial Port to communicate
+        /// </summary>
+        private SerialPort Serial { get; set; }
+
+        /// <summary>
+        /// Whether the connection is open or not
+        /// </summary>
+        private bool IsOpen { get; set; } = false;
+
+        public ArduinoTransmitter(string portName, int baudRate = 9600) => Serial = new SerialPort(portName, baudRate);
+
+        /// <summary>
+        /// Open connection
+        /// </summary>
+        public void Open()
+        {
+            Serial.Open();
+            IsOpen = true;
+        }
+
+        /// <summary>
+        /// Close connection
+        /// </summary>
+        public void Close()
+        {
+            Serial.Close();
+            IsOpen = false;
+        }
+    }
+}
